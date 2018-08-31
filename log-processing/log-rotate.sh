ada0eca32478b5c3a96ad55ecf302378e9df8966
David Vavřička <vavricka.d@gmail.com>
2018-09-01 00:29:47 +0200

    Rsyslog handles all logs from Geth and performs log rotation

---
+++ log-processing/log-rotate.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+LOG_NAME="$1"   # log file's full path - e.g.  "/var/log/txs.log"
+NUM=0
+
+if [ ! -f "$LOG_NAME" ] ; then
+    echo "$0: \"$LOG_NAME\" log file does not exist!"
+    exit 1
+fi
+
+while true
+do
+    if [ ! -f "$LOG_NAME.$NUM" ]
+        then
+        break
+    fi
+    NUM=$((NUM+1))
+done
+
+mv -f $LOG_NAME $LOG_NAME.$((NUM))

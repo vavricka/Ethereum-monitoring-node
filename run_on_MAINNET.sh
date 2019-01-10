f0c64b49d177d3b5700f08a20c390d9cab4dfb22
David Vavricka <vavricka.d@gmail.com>
2019-01-10 21:05:06 +0100

    run script update

--- run_on_MAINNET.sh
+++ run_on_MAINNET.sh
@@ -8,12 +8,8 @@ export DATA_PATH="$PROJ_ROOT/clients-data"
 
 export GETH="$PROJ_ROOT/go-ethereum"
 
-#NOHUP
 nohup "$GETH/build/bin/geth" --syncmode "fast" --identity \
-"INESC-ID-EU-Lisbon" --datadir "$DATA_PATH" \
---cache=6144 --maxpeers "500" &>>"/dev/null" &
-
-#CONSOLE
-#"$GETH/build/bin/geth" --syncmode "fast" --identity \
-#"INESC-ID-EU-Lisbon" --datadir "$DATA_PATH" \
-#--cache=6144 --verbosity 5 console 2>>"/home/dvavricka/data/geth-out.log"
+"INESC" --datadir "$DATA_PATH" \
+--cache=16384 --maxpeers 1500 --maxpendpeers 150 \
+--lightpeers 0 &>>"/dev/null" &
+#--verbosity 5 &>>"/mnt/ssd_1tb/dvavricka/logs/geth-out.log" &

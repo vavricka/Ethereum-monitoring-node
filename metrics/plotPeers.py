e8c941c320fef4890865cf30d773c7504fadb8f3
David Vavricka <vavricka.d@gmail.com>
2019-01-17 18:47:53 +0100

    plotPeers plots Inbound peers and plots it into file

--- metrics/plotPeers.py
+++ metrics/plotPeers.py
@@ -1,19 +1,23 @@
 import pandas as pd                     
 import numpy as np  
+
+# to make it work on server w/o graphics
+import matplotlib as mpl
+mpl.use('Agg')
+
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 PEERS_CSV = "peers.csv"
 
-peers = pd.read_csv(PEERS_CSV, usecols=['LocalTimeStamp', 'Peers'], parse_dates=['LocalTimeStamp'])
+peers = pd.read_csv(PEERS_CSV, usecols=['LocalTimeStamp', 'Peers', 'InboundPeers'], parse_dates=['LocalTimeStamp'])
 peers.rename(columns={'LocalTimeStamp': 'Time'}, inplace=True)
 peers.set_index('Time',inplace=True)
 
-# old peers.csv formating
-#peers = pd.read_csv(PEERS_CSV, usecols=['LocalTimeStamp', 'TypeDependentParam2'], parse_dates=['LocalTimeStamp'])
-#peers.rename(columns={'LocalTimeStamp': 'Time', 'TypeDependentParam2': 'Peers'}, inplace=True)
-#peers.set_index('Time',inplace=True)
-
 peers.plot()
-plt.show()
 
+#show now
+#plt.show()
+
+#save to pdf
+plt.savefig('peers.pdf')

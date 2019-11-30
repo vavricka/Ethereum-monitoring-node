825b0ab3705640365e2d6e393c44b0ac62fc6ee6
David Vavricka <vavricka.d@gmail.com>
2019-11-30 23:05:18 +0100

    cleaning

--- run_client.sh
+++ run_client.sh
@@ -3,12 +3,12 @@
 export PROJ_ROOT="$PWD"
 export DATA_PATH="$PROJ_ROOT/clients-data"
 
-#UNCOMMENT and DO JUST ONE TIME
+#if does not exist
 #mkdir "$DATA_PATH"
 
 export GETH="$PROJ_ROOT/go-ethereum"
 
-nohup "$GETH/build/bin/geth" --syncmode "fast" \
---datadir "$DATA_PATH" \
---cache=6000 --maxpeers 800 --maxpendpeers 80 \
+nohup "$GETH/build/bin/geth" --syncmode "fast" --identity \
+"EMC" --datadir "$DATA_PATH" \
+--cache=75000 --maxpeers 1500 --maxpendpeers 150 \
 --lightpeers 0 --txpool.pricelimit 0 &>>"/dev/null" &
